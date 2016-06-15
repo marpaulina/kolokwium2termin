6ca57dc2be757906ea86469406f20c08c0bacbee
Paulina Marczak <mar.paulina.czak@gmail.com>
2016-06-15 14:04:29 +0200

    Uzupełniony szkielet klasy

--- src/package1/Kolokwium.java
+++ src/package1/Kolokwium.java
@@ -5,23 +5,41 @@ import java.util.ArrayList;
 public class Kolokwium{
 
 	/**
-	* Metoda służy do pomnożenia  trzech liczb z tablicy, gdzie muszą sie znaleźć 
+	* Metoda służy do pomnożenia  trzech liczb z danej tablicy, gdzie muszą sie znaleźć 
 	* wszystkie możliwości mnożenia trzech liczb z danej tablicy.
 	* @param A dana tablica zawierająca liczby.
+	* @return result jest to lista wyników mnożeń.
 	*/ 
 	static ArrayList<Integer>  productOfTriplet(int[] A)
 	{
-	
-		return new ArrayList<Integer>();
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		int N = A.length;
+		for(int P = 0 ;P < N - 2;P++ )
+		{
+			for (int Q = P+1; Q < N -1 ;Q++)
+			{
+				for ( int R = Q+1; R < N ;R++)
+				{
+					result.add(A[P]*A[Q]*A[R]);
+				}
+			}
+		}
+		return result;
 	}
+	
 	/**
 	* Metoda służy do znalezienia maksymalnej wartości z danej listy.
 	* @param result dana lista zawierająca liczby.
+	* @return  max jest to maksymalna wartość z listy.
 	*/ 
 	static int maximumOfResult(ArrayList<Integer> result)
 	{
-		
-		return 0;
-		
+		int max = 0;
+		for (int i : result) {
+			if (max < i) {
+				max = i;
+			}
+		}
+		return max;
 	}
 }
